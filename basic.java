f995625b342264ae3d217bc42c00d5cca843075b
Rajudurairaj <rajuraju06082001@gmail.com>
2024-08-06 09:21:08 +0530

    find ood or even number

--- basic.java
+++ basic.java
@@ -1,1 +1,10 @@
-system.out.print("hello")
+
+public class basic{
+    public static void main(String[] args){
+        int a=20,b=13;
+        if(a % 2 == 0){
+        System.out.println(a+" is a even number");
+    }
+
+    }
+} 
