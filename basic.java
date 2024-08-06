0ce3a97211c6a7c7124c0f1c19c1cb27faa007b8
Rajudurairaj <rajuraju06082001@gmail.com>
2024-08-06 12:14:44 +0530

    find greatest number in three values

--- basic.java
+++ basic.java
@@ -1,12 +1,18 @@
-
+//java "basic.java"find greatest number in three values
 public class basic{
     public static void main(String[] args){
-        int a = 20,b = 70;
-        if(a>b){
-            System.out.println(a+" is greater number"+"\n"+b+" is small number");
+        int a = 20,b = 70,c = 10;
+        if(a>b&&a>c){
+            System.out.println(a+" is greater number");
         }
-        else 
-            System.out.println(b+" is greater number"+"\n"+a+" is small number");
+        else if(b>c&&b>a){
+            System.out.println(b+" is greater number");
+        }
+        else{
+            System.out.println(c+" is greater number");
+        }    
+
+
 
     }
 }
