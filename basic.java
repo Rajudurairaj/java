fca34b27490094736e5e28c91ba141f6f1afd2fd
Rajudurairaj <rajuraju06082001@gmail.com>
2024-08-06 12:40:27 +0530

    if even number ,divide by 2 if odd number ,multiply by 10

--- basic.java
+++ basic.java
@@ -1,14 +1,24 @@
-//Sum of digits
+//if even number ,divide by 2
+//if odd number ,multiply by 10
 public class basic{
     public static void main(String[] args){
-       int n=250,a,sum=0;
-       while(n>0){
-        a=n%10;
-        sum+=a;
-        n/=10;}
-        System.out.println(sum);
-
-
+       int a=60,b=23;
+       if(a%2==0){
+        a/=2;
+        System.out.println(a);
+       }
+       else{
+        a*=10;
+        System.out.println(a);
+       }
+       if(b%2==0){
+        b/=2;
+        System.out.println(b);
+       }
+       else{
+        b*=10;
+        System.out.println(b);
+       }
 
     }
 }
