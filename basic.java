eb03a1bbd423e40b17ff90cb193bf562052e361d
Rajudurairaj <rajuraju06082001@gmail.com>
2024-08-06 12:32:04 +0530

    Sum of digits

--- basic.java
+++ basic.java
@@ -1,9 +1,12 @@
-//Reduce the last two number
+//Sum of digits
 public class basic{
     public static void main(String[] args){
-       int n=250;
-       n/=100;
-       System.out.println(n);
+       int n=250,a,sum=0;
+       while(n>0){
+        a=n%10;
+        sum+=a;
+        n/=10;}
+        System.out.println(sum);
 
 
 
