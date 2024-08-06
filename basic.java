136324a44b6ae62d53e0e07e52abecd2c8c10bc2
Rajudurairaj <rajuraju06082001@gmail.com>
2024-08-06 12:22:39 +0530

    Reduce the last two number

--- basic.java
+++ basic.java
@@ -1,16 +1,9 @@
-//java "basic.java"find greatest number in three values
+//Reduce the last two number
 public class basic{
     public static void main(String[] args){
-        int a = 20,b = 70,c = 10;
-        if(a>b&&a>c){
-            System.out.println(a+" is greater number");
-        }
-        else if(b>c&&b>a){
-            System.out.println(b+" is greater number");
-        }
-        else{
-            System.out.println(c+" is greater number");
-        }    
+       int n=250;
+       n/=100;
+       System.out.println(n);
 
 
 
