986d274397d16bb40f3bdc64158a3f99a83caeb3
Rajudurairaj <rajuraju06082001@gmail.com>
2024-08-06 11:12:01 +0530

    find greatest and small number

--- basic.java
+++ basic.java
@@ -3,10 +3,10 @@ public class basic{
     public static void main(String[] args){
         int a = 20,b = 70;
         if(a>b){
-        System.out.println(a+" is greater number");
+            System.out.println(a+" is greater number"+"\n"+b+" is small number");
         }
         else 
-        System.out.println(b+" is greater number");
+            System.out.println(b+" is greater number"+"\n"+a+" is small number");
 
     }
 }
