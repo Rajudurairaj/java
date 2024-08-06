e34b923c6b70c261e416ba44f891f8922354b926
Rajudurairaj <rajuraju06082001@gmail.com>
2024-08-06 14:54:34 +0530

    for loop

--- basic.java
+++ basic.java
@@ -1,42 +1,14 @@
-//if even number ,divide by 2
-//if odd number ,multiply by 10
-public class basic{
-    public static void main(String[] args){
-       int r=6;
-       switch(r){
-        case 0:
-            System.out.println("The number is Zero");
-            break;
-        case 1:
-            System.out.println("The number is One");
-            break;
-        case 2:
-            System.out.println("The number is Two");
-            break;
-        case 3:
-            System.out.println("The number is Three");
-            break;
-        case 4:
-            System.out.println("The number is Four");
-            break;
-        case 5:
-            System.out.println("The number is Five");
-            break;
-        case 6:
-            System.out.println("The number is Six");
-            break;
-        case 7:
-            System.out.println("The number is Seven");
-            break;
-        case 8:
-            System.out.println("The number is Eight");
-            break;
-        case 9:
-            System.out.println("The number is Nine");
-            break;
-        default:
-            System.out.println("Invalied number");
-       }
+//For loop
 
+public class basic{
+    void more(int a){
+        for(int i=0;i<=a;i++){
+            System.out.println("HELLO WORLD");
+        }
+    }
+    public static void main(String[] args){
+       int n=6;
+       basic obj = new basic();
+       obj.more(n);
     }
 }
