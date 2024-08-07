4647b6d337ef03df92652a3095bcfb038c2b9bc3
Rajudurairaj <rajuraju06082001@gmail.com>
2024-08-07 14:47:23 +0530

    function Math.pow() and type casting double to int

--- basic.java
+++ basic.java
@@ -1,21 +1,28 @@
-//function Math.pow() for squre
+//function Math.pow() and type casting double to int
 import java.util.Scanner;
 public class basic{
-     double more(Double a,Double b){
-        long x,y;
-        double s;
-        s=Math.pow(a,b);
+     int more(int a,int b){
+        /*Using TypeCasting
+        Using Double.intValue() method
+        Using Math.round() method 
+        double data = 3452.345
+        int value = (int)data;*/
+        int s;
+        s=(int)Math.pow(a,b);
+
         return s;
     }
     public static void main(String[] args){
         Scanner myobj = new Scanner(System.in);
         System.out.println("Enter numbers");
-        double p = myobj.nextLong();
-        double n= myobj.nextLong();
+        int p = myobj.nextInt();
+        int n= myobj.nextInt();
         basic obj = new basic();
         System.out.println(obj.more(p,n));
     }
-}
+} 
+
+
 
 
 
