bc8a817ab0a04cd10fe89d87b2e3064a595a6030
Rajudurairaj <rajuraju06082001@gmail.com>
2024-08-07 15:41:37 +0530

    System.out.println(p.length()); .lenth() command print the length of the string

--- basic.java
+++ basic.java
@@ -1,11 +1,11 @@
-//System.out.println(p.charAt(0)); 0 mean pritnt that index value only
+//System.out.println(p.length()); .lenth() command print the length of the string
 import java.util.Scanner;
 public class basic{
     public static void main(String[] args){
         Scanner myobj = new Scanner(System.in);
         System.out.println("Enter string");
         String p = myobj.nextLine();
-        System.out.println(p.charAt(0));
+        System.out.println(p.length());
 }
 } 
  
