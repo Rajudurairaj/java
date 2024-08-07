4be8d717f1887bf4507299c41d631ad71e978211
Rajudurairaj <rajuraju06082001@gmail.com>
2024-08-07 11:05:57 +0530

    while loop print digits with run time input

--- basic.java
+++ basic.java
@@ -1,5 +1,5 @@
-//while loop print sum of digits
-
+//while loop print digits with run time input
+import java.util.Scanner;
 public class basic{
      long more(long a){
         long x,y,s=0;
@@ -11,8 +11,10 @@ public class basic{
         return s;
     }
     public static void main(String[] args){
+        Scanner myobj = new Scanner(System.in);
+        long p = myobj.nextLong();
        long n=123456789;
        basic obj = new basic();
-       System.out.println(obj.more(n));
+       System.out.println(obj.more(p));
     }
 }
