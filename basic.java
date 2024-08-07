df557139d14dabc97bf47099cec626d10d03559f
Rajudurairaj <rajuraju06082001@gmail.com>
2024-08-07 15:49:14 +0530

    System.out.println(p.substring(0,2)); print the starting index to endindex

--- basic.java
+++ basic.java
@@ -1,11 +1,11 @@
-//System.out.println(p.trim()); .trim() remove the spces in front
+//System.out.println(p.substring(0,2)); print the starting index to endindex
 import java.util.Scanner;
 public class basic{
     public static void main(String[] args){
         Scanner myobj = new Scanner(System.in);
         System.out.println("Enter string");
         String p = myobj.nextLine();
-        System.out.println(p.trim());
+        System.out.println(p.substring(0,2));
 }
 } 
  
