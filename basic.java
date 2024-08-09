0e996dd571a4c380628a841d123330682b66a8d5
Rajudurairaj <rajuraju06082001@gmail.com>
2024-08-09 11:35:02 +0530

    Array

--- basic.java
+++ basic.java
@@ -1,15 +1,20 @@
-//System.out.println(p.substring(0,2)); print the starting index to endindex
+// Array
 import java.util.Scanner;
-public class basic{
-    public static void main(String[] args){
-        Scanner myobj = new Scanner(System.in);
-        System.out.println("Enter string");
-        String p = myobj.nextLine();
-        System.out.println(p.substring(0,2));
-}
-} 
+class basic{
+void display(String s[],int a){
+    for(int j=0;j<a;j++){
+System.out.println(s[j]);
+    }
  
-
-
-
-
+ }    public static void main(String[] args) {
+    Scanner myobj = new Scanner(System.in);
+    //int v = myobj.nextInt();
+    String[] str= new String [3];
+    for(int i=0;i<3;i++){
+            str[i] = myobj.nextLine();    }
+            
+            basic obj = new basic();
+            obj.display(str,3);
+    System.out.println("result");
+    }
+}
