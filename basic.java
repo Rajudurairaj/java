aee6815e640ea3cc35083b0f95bdba647c270ffd
Rajudurairaj <rajuraju06082001@gmail.com>
2024-08-09 12:37:43 +0530

    find value length and digits add+length after print output using keyword integer.toString()

--- basic.java
+++ basic.java
@@ -1,4 +1,4 @@
-// find value length and separate the digits add+length after print the output
+// find value length and separate the digits add+length after print the output using keyword integer.tiString()
 import java.util.Scanner;
 class basic{
 void display(int a){
@@ -12,14 +12,11 @@ System.out.println(a);
     String vstr = Integer.toString(v);
     int l = vstr.length();
     while(v != 0){
-            x+=v%10;
+            x=(x+l)+(v%10);
             v/=10;
-            i++;
                }
-               x+=(i*i);
-            
             basic obj = new basic();
             obj.display(x);
-    System.out.println("result"+l);
+    System.out.println("result");
     }
 }
