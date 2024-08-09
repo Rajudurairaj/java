144adb358493353939c0f7fd46e034f5f8f53ba6
Rajudurairaj <rajuraju06082001@gmail.com>
2024-08-09 11:47:11 +0530

    Array sum integer

--- basic.java
+++ basic.java
@@ -1,20 +1,21 @@
-// Array
+// Array sum integer
 import java.util.Scanner;
 class basic{
-void display(String s[],int a){
+int display(int s[],int a){
+    int u=0;
     for(int j=0;j<a;j++){
-System.out.println(s[j]);
+    u+=s[j];
     }
+    return u;
  
  }    public static void main(String[] args) {
     Scanner myobj = new Scanner(System.in);
     //int v = myobj.nextInt();
-    String[] str= new String [3];
-    for(int i=0;i<3;i++){
-            str[i] = myobj.nextLine();    }
+    int[] str= new int [5];
+    for(int i=0;i<5;i++){
+            str[i] = myobj.nextInt();    }
             
             basic obj = new basic();
-            obj.display(str,3);
-    System.out.println("result");
+    System.out.println("result is "+obj.display(str,5));
     }
 }
